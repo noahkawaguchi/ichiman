a0e3259c4a389da3ffc1485addc4eed9eda4750a
noahkawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-08-17 18:21:30 -0400

    Flesh out 'Start a new habit' mode functionality

--- main_streamlit.py
+++ main_streamlit.py
@@ -5,7 +5,7 @@ from glob import glob
 import streamlit as st
 import pandas as pd
 
-from scripts2.helpers import show_all_data_info, update_data, up_to_date_download
+from scripts2.helpers import show_all_data_info, update_data, up_to_date_download, update_data2
 
 st.set_page_config(
     page_title='ICHIMAN | duration-based habit tracker',
@@ -31,19 +31,42 @@ with c2:
 st.divider()
 
 if mode == 'Start a new habit':
-    c1, c2 = st.columns([1, 2])
-    with c1:
-        st.write('### Start a new habit')
-        st.write('A journey of ten thousand hours begins with a single day.')
-    with c2:
-        new_habit_name = st.text_input('Enter the name of the habit here:')
-        if new_habit_name:
-            st.write('New habit:',new_habit_name)
+
+    # Initialize a persistent session_state variable so the time entry 
+    # interface can stay displayed
+    if 'starting_new' not in st.session_state:
+        st.session_state.starting_new = False
+
+    st.write('### Start a new habit')
+    st.write('A journey of ten thousand hours begins with a single day.')
+
+    # The button clears other habits and displays the duration entry 
+    # interface 
+    start_button = st.button(
+        "Let's go!", 
+        on_click=lambda: st.session_state.clear(),
+        )
+    if start_button:
+        st.session_state.starting_new = True
+
+    st.divider()
+
+    # Prompt the user to enter data for today
+    if st.session_state.starting_new:
+        today = dt.datetime.today().date()
+        empty_df = pd.DataFrame(columns=['date', 'duration'])
+        new_df, up_to_date = update_data2(empty_df, today)
+
+        # Display a download option and data insights
+        if up_to_date:
+            up_to_date_download(new_df)
+            st.divider()
+            show_all_data_info(new_df)
 
 elif mode == 'Track an existing habit':
     
     # Show a basic file upload interface
-    c1, c2 = st.columns([1, 2])
+    c1, c2 = st.columns([1,2])
     with c1:
         st.write('### Track an existing habit')
         st.write('Nice to see you again.')
@@ -51,13 +74,15 @@ elif mode == 'Track an existing habit':
         uploaded_file = st.file_uploader(
             'Upload a CSV file previously created with this app:', 
             type='csv',
+            on_change=lambda: st.session_state.clear()
             )
+    
     st.divider()
 
     # Read in the user's CSV file
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-
+        
         # Initialize session_state variables 
         if 'tracking_df' not in st.session_state:
             st.session_state.tracking_df = df.copy()
@@ -73,16 +98,20 @@ elif mode == 'Track an existing habit':
             pd.to_timedelta(st.session_state.tracking_df['duration'])
             )
 
-        # Update the session_state DataFrame
-        if not st.session_state.up_to_date:
+        # Check if the data is up to date
+        latest_date = st.session_state.tracking_df['date'].iloc[-1].date()
+        today = dt.datetime.today().date()
+        if latest_date < today:
+            # Update the session_state DataFrame
+            next_date = latest_date + dt.timedelta(days=1)
             st.session_state.tracking_df, st.session_state.up_to_date = (
-                update_data(df)
+                update_data2(df, next_date)
                 )
+        else:
+            st.session_state.up_to_date = True
         
-        # Show the download option and the data insights for the 
-        # up-to-date data.
-        # This needs to be another if, not an else, due to the way 
-        # streamlit reruns the code from top to bottom.
+        # Once the data is up to date show the download option and the 
+        # data insights.
         if st.session_state.up_to_date:
             up_to_date_download(st.session_state.tracking_df)
             st.divider()
--- scripts2/helpers.py
+++ scripts2/helpers.py
@@ -31,6 +31,122 @@ def show_all_data_info(df: pd.DataFrame) -> None:
     with st.expander('Show all days'):
         data.display_data(df_copy)
 
+def update_data2(
+        df: pd.DataFrame, date_cursor: dt.date
+        ) -> Tuple[pd.DataFrame, bool]:
+    """Given the currently recorded dates and durations, prompt 
+    the user to enter data for each day up to the present day. 
+    With each iteration, return the data and a boolean of whether or 
+    not the data is fully up to date. (This complexity is necessary 
+    because of Streamlit's repeated top-down execution.)
+    """
+    # Initialize session_state variables for the parameters
+    if 'new_df' not in st.session_state:
+        st.session_state.new_df = df.copy()
+    if 'date_cursor' not in st.session_state:
+        st.session_state.date_cursor = date_cursor
+
+    # Ensure the 'date' column is of datetime type and the 
+    # 'duration' column of timedelta type
+    st.session_state.new_df['date'] = pd.to_datetime(st.session_state.new_df['date'])
+    st.session_state.new_df['duration'] = pd.to_timedelta(st.session_state.new_df['duration'])
+
+    # Check if the data needs to be updated
+    today = dt.datetime.today().date()
+    if st.session_state.date_cursor > today:
+        up_to_date = True
+        return st.session_state.new_df, up_to_date
+    else:
+        st.write('### Enter time spent:')
+
+        # Collect the hours and minutes from the user for the date in 
+        # question
+        c1, c2 = st.columns(2)
+        with c1:
+            new_duration_hours = st.number_input(
+                'Hours (type or use − +):',
+                min_value=0,
+                max_value=23,
+                value=0,
+                step=1,
+                format='%d',
+                label_visibility='collapsed'
+            )
+        with c2:
+            st.write('##### hour(s) and')
+        c1, c2 = st.columns(2)
+        with c1:
+            new_duration_minutes = st.number_input(
+                'Minutes (type or use − +):',
+                min_value=0,
+                max_value=59,
+                value=0,
+                step=1,
+                format='%d',
+                label_visibility='collapsed'
+            )
+        with c2:
+            st.write('##### minute(s)')
+
+        # Convert the user's input into a DataFrame
+        new_duration = dt.timedelta(hours=new_duration_hours,
+                                    minutes=new_duration_minutes)
+        new_row = pd.DataFrame({'date': [st.session_state.date_cursor], 'duration': [new_duration]})
+        new_row['date'] = pd.to_datetime(new_row['date'])
+
+        # When the user presses save, save the data to the 
+        # session_state DataFrame
+        c1, c2 = st.columns(2)
+        with c1:
+            save_button = st.button('Save')
+        if save_button:
+            st.session_state.new_df = pd.concat(
+                [st.session_state.new_df, new_row], 
+                ignore_index=True,
+                )
+
+            # Temporarily display a confirmation message that the data 
+            # for the given day has been saved
+            short_date = st.session_state.date_cursor.strftime('%b %e')
+            placeholder = st.empty()
+            placeholder.text(f'{short_date} updated!')
+            sleep(1.75)
+            placeholder.empty()
+
+            # Point to the following day
+            st.session_state.date_cursor += dt.timedelta(days=1)
+
+        # Display the date for which the user should enter data. (This 
+        # has to be placed here in the code so that it will reflect the 
+        # correct day as soon as it updates.)
+        with c2:
+            if st.session_state.date_cursor <= today:
+                formatted_date = st.session_state.date_cursor.strftime('%a, %b %e, %Y')
+                st.write(f'##### for {formatted_date}')
+
+        # While the data is not yet up to date, show the latest data 
+        # being updated
+        if st.session_state.date_cursor <= today:
+            if len(st.session_state.new_df) > 0:
+                st.divider()
+                reversed_df = st.session_state.new_df.iloc[::-1]
+                data.display_data(reversed_df)
+
+        # Once the data is up to date, show a button to end data entry 
+        else:
+            st.write('All up to date!')
+            done_button = st.button('See results')
+            if done_button:
+                up_to_date = True
+                return st.session_state.new_df, up_to_date
+        
+        # This is here because the caller function needs to repeatedly 
+        # get valid return values even before any user input.
+        up_to_date = False
+        return st.session_state.new_df, up_to_date
+
+
+# Get rid of this one soon
 def update_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, bool]:
     """Given the currently recorded dates and durations, prompt 
     the user to enter data for each day up to the present day. 
@@ -120,7 +236,9 @@ def update_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, bool]:
             # Point to the following day
             st.session_state.next_day += dt.timedelta(days=1)
 
-        # Display the date for which the user should enter data
+        # Display the date for which the user should enter data. (This 
+        # has to be placed here in the code so that it will reflect the 
+        # correct day as soon as it updates.)
         with c2:
             if st.session_state.new_df['date'].iloc[-1].date() < today:
                 st.session_state.formatted_date = st.session_state.next_day.strftime('%a, %b %e, %Y')
@@ -142,12 +260,13 @@ def update_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, bool]:
                 return st.session_state.new_df, up_to_date
         
         # This is here because the caller function needs to repeatedly 
-        # get valid return values.
+        # get valid return values even before any user input.
         up_to_date = False
         return st.session_state.new_df, up_to_date
 
 def up_to_date_download(df: pd.DataFrame) -> None:
-    """Format the updated DataFrame and create a CSV file.
+    """Display the updated status of the data.
+    Format the DataFrame and create a CSV file.
     Display an interface to download the file.
     """
     # Copy the DataFrame and remove '0 days' from the 'duration' values
@@ -160,11 +279,16 @@ def up_to_date_download(df: pd.DataFrame) -> None:
     # Create a CSV file with the formatted data 
     csv = remove_days.to_csv(index=False)
 
-    # Display a basic interface to download the CSV file 
+    # Display the up-to-date status of the data
     c1, c2 = st.columns(2)
     with c1:
-        latest_date = df['date'].iloc[-1].date()
-        st.write(f'#### This data is up to date! ({latest_date.strftime('%B %e')})')
+        earliest_date = df['date'].iloc[0].date().strftime('%a, %b %e, %Y')
+        latest_date = df['date'].iloc[-1].date().strftime('%a, %b %e, %Y')
+        st.write(f'#### This data is up to date from {earliest_date} to {latest_date}')
+        if earliest_date == latest_date:
+            st.write("(You only have one day's worth of data)")
+
+    # Display a basic interface to download the CSV file 
     with c2:
         c1, c2 = st.columns(2)
         with c1:
